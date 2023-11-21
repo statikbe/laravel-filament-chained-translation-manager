230f4462b3b174d2c2871d03468a721bd2658e20
François Clapuyt <fclapuyt@gmail.com>
2023-11-21 15:26:28 +0100

    add French translations

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Gestionnaire de traductions',
+    'navigation_group' => 'Configuration',
+    'search_term_placeholder' => 'Rechercher une traduction',
+    'selected_groups_placeholder' => 'Sélectionner un groupe',
+    'selected_languages_placeholder' => 'Sélectionner une langue',
+    'only_show_missing_translations_lbl' => 'Afficher seulement les traductions manquantes',
+    'error_no_translations_for_filters' => 'Ajustez les filtres, aucune traduction ne correspond à votre requête!',
+    'error_no_translation_loaded' => 'Aucune traduction n\'a été trouvée. Vérifiez votre dossier <pre>lang</pre>',
+    'previous_page' => 'Précédent',
+    'next_page' => 'Suivant',
+    'missing_translation' => 'La traduction est manquante',
+    'filter_action' => 'Filtrer',
+    'filter_results' => ':filtered éléments filtrés sur :total traductions.',
+    'filter_results_missing_translations' => ':missing éléments n\'ont pas de traduction (:percent%).',
+    'saved_translation' => 'Traduction sauvegardée',
+];
