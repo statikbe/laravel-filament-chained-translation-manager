cae8fe2bd1df3028f8cf7c938c71409959fe9146
Sten Govaerts <sten.govaerts@gmail.com>
2025-11-12 23:45:40 +0100

    Added a button to the editor to toggle multi-line mode by showing a textarea or input + aria-label/title on the buttons + translations

--- resources/views/livewire/translation-edit-form.blade.php
+++ resources/views/livewire/translation-edit-form.blade.php
@@ -9,22 +9,27 @@
                 class="flex items-center"
                 x-data="{
                     editing: false,
-                    openForm(){
+                    multiLineMode: {{ substr_count($translations[$locale] ?? '', "\n") > 0 ? 'true' : 'false' }},
+                    openForm() {
                         $dispatch('close-forms');
                         this.editing = true;
                         $nextTick(() => {
                             setTimeout(() => {
-                                $refs.input.focus();
+                                if (this.multiLineMode) {
+                                    $refs.multilineInput.focus();
+                                } else {
+                                    $refs.input.focus();
+                                }
                             }, 50); //Adding a small delay makes this way more consistent
                         });
                     },
-                    closeWithSave(){
+                    closeWithSave() {
                         if (this.editing){
                             this.closeEdit();
                             $wire.save(this.locale);
                         }
                     },
-                    closeWithCancel(){
+                    closeWithCancel() {
                         if (this.editing){
                             this.closeEdit();
                             $wire.cancel();
@@ -33,7 +38,10 @@
                     closeEdit() {
                         this.editing = false;
                     },
-                    locale: '{{ $locale }}'
+                    toggleMultiLine() {
+                        this.multiLineMode = !this.multiLineMode;
+                    },
+                    locale: '{{ $locale }}',
                 }"
                 @click.outside="closeWithSave"
                 @close-forms.window="closeWithSave()"
@@ -60,21 +68,46 @@
                 </x-filament::link>
                 <div
                     class="w-full flex items-center space-x-2"
-                    x-show="editing"
-                >
+                    x-show="editing">
                     <form @submit.prevent="closeWithSave" class="w-full">
                         <input
                             wire:model.defer="translations.{{ $locale }}"
-                            class="{{
-                                'block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 border-gray-300 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500 '
-                            }}"
+                            class="block w-full p-2 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 border-gray-300 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500"
                             id="{{ $id }}"
                             type="text"
                             x-ref="input"
+                            x-show="!multiLineMode"
+                            x-bind:disabled="multiLineMode"
                         >
+                        <textarea
+                            wire:model.defer="translations.{{ $locale }}"
+                            class="block w-full p-2 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 border-gray-300 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500"
+                            id="{{ $id }}"
+                            rows="3"
+                            x-ref="multilineInput"
+                            x-show="multiLineMode"
+                            x-bind:disabled="!multiLineMode"></textarea>
                     </form>
                     <div class="flex items-center align-center">
-                        <button @click="closeWithCancel">
+                        <button @click="toggleMultiLine"
+                                title="@lang('filament-translation-manager::messages.toggle_multi_line_btn')"
+                                aria-label="@lang('filament-translation-manager::messages.toggle_multi_line_btn')">
+                            <x-filament::icon
+                                x-show="!multiLineMode"
+                                alias="filament-chained-translation-manager::enable-multi-line"
+                                icon="heroicon-o-bars-arrow-down"
+                                class="w-5 h-5 text-primary-500"/>
+                            <x-filament::icon
+                                x-show="multiLineMode"
+                                alias="filament-chained-translation-manager::disable-multi-line"
+                                icon="heroicon-o-bars-arrow-up"
+                                class="w-5 h-5 text-primary-500"/>
+                        </button>
+                    </div>
+                    <div class="flex items-center align-center">
+                        <button @click="closeWithCancel"
+                                title="@lang('filament-translation-manager::messages.cancel_translation_btn')"
+                                aria-label="@lang('filament-translation-manager::messages.cancel_translation_btn')">
                             <x-filament::icon
                                 alias="filament-chained-translation-manager::cancel-translation"
                                 icon="heroicon-o-x-mark"
